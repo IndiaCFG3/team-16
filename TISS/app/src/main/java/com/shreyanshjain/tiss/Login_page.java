019c27759d0cd3260b569b8c4dbb611d11c0cbbe
Saikeerthana.Akella <saikeerthana2001@gmail.com>
2020-08-16 12:18:28 +0530

    updated login page(xml)

--- TISS/app/src/main/java/com/shreyanshjain/tiss/Login_page.java
+++ TISS/app/src/main/java/com/shreyanshjain/tiss/Login_page.java
@@ -3,8 +3,11 @@ package com.shreyanshjain.tiss;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class Login_page extends AppCompatActivity {
+    private Button otp_btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
