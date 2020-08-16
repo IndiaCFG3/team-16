5f009880eb013f0a90dea5569b5d165714d610e0
Aarush Gupta <50203165+aarush7@users.noreply.github.com>
2020-08-16 20:25:28 +0530

    Update urls.py

--- webapp/tiss/tissapp/urls.py
+++ webapp/tiss/tissapp/urls.py
@@ -5,13 +5,13 @@ from .views import StudentView,InstructorView,CourseView,ModuleView,SubjectView,
 
 urlpatterns=[
     path('student/',StudentView.as_view()),
-    path('student/<uuid:pk>',StudentupdateView.as_view()),
+    path('student/<int:pk>',StudentupdateView.as_view()),
     path('instructor/',InstructorView.as_view()),
-    path('instructor/<uuid:pk>',InstructorupdateView.as_view()),
+    path('instructor/<int:pk>',InstructorupdateView.as_view()),
     path('module/',ModuleView.as_view()),
-    path('module/<uuid:pk>', ModuleupdateView.as_view()),
+    path('module/<int:pk>', ModuleupdateView.as_view()),
     path('subject/',SubjectView.as_view()),
-    path('subject/<uuid:pk>', SubjectupdateView.as_view()),
+    path('subject/<int:pk>', SubjectupdateView.as_view()),
     path('course/',CourseView.as_view()),
-    path('course/<uuid:pk>', CourseupdateView.as_view())
+    path('course/<int:pk>', CourseupdateView.as_view())
     ]
